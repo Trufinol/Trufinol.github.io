ba5e99f0815f70fd6f5a8a85559d0164310e9120
Nikolay Verteba <veseliy07@gmail.com>
2016-05-22 13:50:31 +0300

    resume bars
    get bars work after load

--- VC_site/js/jquery-custom.js
+++ VC_site/js/jquery-custom.js
@@ -1,39 +1,81 @@
 ﻿$(document).ready(function () {
 
-    $('#btn1').click(function () {
-        $.ajax({
-            url: "http://localhost:3000/profile.html",
-            cache: true,
-            success: function (html) {
-                $(".cont-wrap").html(html);
-            }
-        });
+ $(document).ajaxComplete(function(){
+
+        function barAnimate(id) {
+
+            idVal = id.data('value');
+            TweenMax.to(id,1,{width:idVal+'%',ease: Power1.easeIn});
+
+            setInterval(function() {
+                var progr = $('.progress').css('width');
+                var length =  id.css('width'),
+                perc = Math.round((parseInt(length)/parseInt(progr))*100);
+                id.text(perc+'%');
+            }, 50);
+        }
+
+        barAnimate($('#bar-1'));
+        barAnimate($('#bar-2'));
+        barAnimate($('#bar-3'));
+        barAnimate($('#bar-4'));
+});
+//hrefs
+$('#profile').click(function () {
+    $.ajax({
+        url: "http://localhost:3000/profile.html",
+        success: function (html) {
+            $(".cont-wrap").html(html);
+        }
     });
-    $('#btn2').click(function () {
-        $.ajax({
-            url: "http://localhost:3000/resume.html",
-            cache: true,
-            success: function (html) {
-                $(".cont-wrap").html(html);
-            }
-        });
+});
+$('#resume').click(function () {
+    $.ajax({
+        url: "http://localhost:3000/resume.html",
+        success: function (html) {
+            $(".cont-wrap").html(html);
+            $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+        }
     });
-    $('#btn3').click(function () {
-        $.ajax({
-            url: "http://localhost:3000/portfolio.html",
-            cache: true,
-            success: function (html) {
-                $(".cont-wrap").html(html);
-            }
-        });
+});
+$('#portfolio').click(function () {
+    $.ajax({
+        url: "http://localhost:3000/portfolio.html",
+        success: function (html) {
+            $(".cont-wrap").html(html);
+            $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+        }
     });
-    $('#btn4').click(function () {
-        $.ajax({
-            url: "http://localhost:3000/contacts.html",
-            cache: true,
-            success: function (html) {
-                $(".cont-wrap").html(html);
-            }
-        });
+});
+$('#contacts').click(function () {
+    $.ajax({
+        url: "http://localhost:3000/contacts.html",
+        success: function (html) {
+            $(".cont-wrap").html(html);
+            $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+        }
     });
+});
+
+//bars
+
+// var bar1 = $('#bar-1').data('value');
+// TweenMax.to($('#bar-1'),1,{width:bar1+'%',ease: Circ.easeIn});
+// function perc() {
+//     var progr = $('.progress').css('width');
+//     var length =  $('#bar-1').css('width'),
+//     perc = Math.round((parseInt(length)/parseInt(progr))*100);
+//     $('#bar-1').text(perc+'%');
+// }
+// perc();
+// setInterval(perc, 50);
+// });
+
+
 });
