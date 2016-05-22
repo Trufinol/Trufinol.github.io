31b69287ff6538eca04934ee96f0ad2afd92f9bc
Nikolay Verteba <veseliy07@gmail.com>
2016-05-22 10:00:34 +0300

    backup

---
+++ VC_site/js/jquery-custom.js
@@ -0,0 +1,39 @@
+﻿$(document).ready(function () {
+
+    $('#btn1').click(function () {
+        $.ajax({
+            url: "http://localhost:3000/profile.html",
+            cache: true,
+            success: function (html) {
+                $(".cont-wrap").html(html);
+            }
+        });
+    });
+    $('#btn2').click(function () {
+        $.ajax({
+            url: "http://localhost:3000/resume.html",
+            cache: true,
+            success: function (html) {
+                $(".cont-wrap").html(html);
+            }
+        });
+    });
+    $('#btn3').click(function () {
+        $.ajax({
+            url: "http://localhost:3000/portfolio.html",
+            cache: true,
+            success: function (html) {
+                $(".cont-wrap").html(html);
+            }
+        });
+    });
+    $('#btn4').click(function () {
+        $.ajax({
+            url: "http://localhost:3000/contacts.html",
+            cache: true,
+            success: function (html) {
+                $(".cont-wrap").html(html);
+            }
+        });
+    });
+});
