500dbf26d9ed8c68db82a5ee00472087f70a494e
Nikolay <veseliy07@gmail.com>
2016-05-25 19:52:25 +0300

    onscroll

--- VC_site/js/jquery-custom.js
+++ VC_site/js/jquery-custom.js
@@ -1,86 +1,100 @@
-﻿$(document).ready(function () {
+﻿$(function () {
 
-    var $contwrap = $(".cont-wrap");
-
+    var $content = $(".content");
+    var $contwrap = $('.cont-wrap');
+    $contwrap.css('visibility', 'visible');
     function profAnim() {
-       $contwrap.fadeIn(400);
-       TweenMax.fromTo($('h1,h2,.profile > p'),0.5,{x:200},{x:0, ease: Back.easeOut.config(1)});
-       TweenMax.fromTo($('.step-light,.step-bold'),0.8,{x:-200},{x:0, ease: Back.easeOut.config(1)},0.3);
-       TweenMax.staggerFrom($('.b-info'),0.5,{opacity:0, scale:0} ,0.05);
-   };
+        $content.fadeIn(400);
+        TweenMax.fromTo($('h1,h2,.profile > p'), 0.8, { x: 200 }, { x: 0, ease: Back.easeOut.config(1) });
+        TweenMax.fromTo($('.step-light,.step-bold'), 0.8, { x: -200 }, { x: 0, ease: Back.easeOut.config(1) }, 0.3);
+        TweenMax.staggerFrom($('.b-info'), 0.6, { opacity: 0, scale: 0 }, 0.08);
+    };
 
-   profAnim();
+    profAnim();
 
-//hrefs
-$('#profile').click(function (e) {
-    e.preventDefault();
-    $contwrap.fadeOut(300,function(){
+    //hrefs
+    $('#profile').click(function (e) {
+        e.preventDefault();
 
-        $.ajax({
-            url: "http://localhost:3000/profile.html",
-            success: function (html) {
+        $content.fadeOut(300, function () {
 
-               $contwrap.html(html);
-               profAnim();
-           }
-       });
-    });
-});
-
-$('#resume').click(function (e) {
-    e.preventDefault();
-    $contwrap.fadeOut(300,function(){
-        $.ajax({
-            url: "http://localhost:3000/resume.html",
-            success: function (html) {
-
-               $contwrap.html(html);
-               $contwrap.fadeIn(300);
-
-           }
-       });
-        $(document).ajaxComplete(function(){
-
-            function barAnimate(id) {
-
-                idVal = id.data('value');
-                TweenMax.to(id,1,{width:idVal+'%',ease: Power1.easeIn});
-
-                setInterval(function() {
-                    var progr = $('.progress').css('width');
-                    var length =  id.css('width'),
-                    perc = Math.round((parseInt(length)/parseInt(progr))*100);
-                    id.text(perc+'%');
-                }, 50);
-            }
-
-            barAnimate($('#bar-1'));
-            barAnimate($('#bar-2'));
-            barAnimate($('#bar-3'));
-            barAnimate($('#bar-4'));
+            $.ajax({
+                url: "http://localhost:3000/profile.html",
+                success: function (html) {
+                    $contwrap.html(html);
+                    profAnim();
+                }
+            });
         });
     });
-});
 
-$('#portfolio').click(function (e) {
-    e.preventDefault();
-    $.ajax({
-        url: "http://localhost:3000/portfolio.html",
-        success: function (html) {
-            $contwrap.html(html);
-            $('html, body')
-        }
+    $('#resume').click(function (e) {
+        e.preventDefault();
+
+        $content.fadeOut(300, function () {
+            $.ajax({
+                url: "http://localhost:3000/resume.html",
+                success: function (html) {
+                    $contwrap.html(html);
+                    $content.fadeIn(300);
+
+                }
+            });
+            $(document).ajaxComplete(function () {
+
+                function barAnimate(id) {
+
+                    idVal = id.data('value');
+                    TweenMax.to(id, 1, { width: idVal + '%', ease: Power1.easeIn });
+
+                    setInterval(function () {
+                        var progr = $('.progress').css('width');
+                        var length = id.css('width'),
+                        perc = Math.round((parseInt(length) / parseInt(progr)) * 100);
+                        id.text(perc + '%');
+                    }, 50);
+                }
+                if (window.innerHeight < 800) {
+                $(document).bind('scroll', function (e) {
+                    var scrollOffset = $(document).scrollTop();
+                    var containerOffset = $('#skillbars').offset().top - window.innerHeight;
+                    if (scrollOffset > containerOffset) {
+                        barAnimate($('#bar-1'));
+                        barAnimate($('#bar-2'));
+                        barAnimate($('#bar-3'));
+                        barAnimate($('#bar-4'));
+                        $(document).unbind('scroll');
+                    }
+                });
+                } else {
+                    barAnimate($('#bar-1'));
+                    barAnimate($('#bar-2'));
+                    barAnimate($('#bar-3'));
+                    barAnimate($('#bar-4'));
+                }
+            });
+        });
     });
-});
-$('#contacts').click(function (e) {
-    e.preventDefault();
-    $.ajax({
-        url: "http://localhost:3000/contacts.html",
-        success: function (html) {
-            $contwrap.html(html);
-            $('html, body')
-        }
+
+    $('#portfolio').click(function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: "http://localhost:3000/portfolio.html",
+            success: function (html) {
+                $contwrap.html(html);
+
+            }
+        });
     });
-});
+    $('#contacts').click(function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: "http://localhost:3000/contacts.html",
+            success: function (html) {
+                $contwrap.html(html);
+
+            }
+        });
+    });
 
 });
