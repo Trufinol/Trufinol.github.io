c2144f0b472092a33173a327d36c7015a310aa24
Nikolay <veseliy07@gmail.com>
2016-05-26 18:50:53 +0300

    Add files via upload

---
+++ js/jquery-custom.js
@@ -0,0 +1,111 @@
+﻿$(function () {
+    var $preloader = $('#preloader');
+    var $content = $(".content");
+    var $contwrap = $('.cont-wrap');
+    $preloader.fadeOut(200);
+
+    function profAnim() {
+        $contwrap.css('visibility', 'visible');
+        $content.fadeIn(400);
+        TweenMax.fromTo($('h1,h2,.profile > p'), 0.8, { x: 200 }, { x: 0, ease: Back.easeOut.config(1) });
+        TweenMax.fromTo($('.step-light,.step-bold'), 0.8, { x: -200 }, { x: 0, ease: Back.easeOut.config(1) }, 0.3);
+        TweenMax.staggerFrom($('.b-info'), 0.6, { opacity: 0, scale: 0 }, 0.08);
+
+    };
+
+    setTimeout(profAnim(), 5000);
+
+    //hrefs
+    $('#profile').click(function (e) {
+        e.preventDefault();
+
+        $content.fadeOut(300, function () {
+
+            $.ajax({
+                url: "https://trufinol.github.io/VCard/Projects/profile.html",
+                success: function (html) {
+                    $contwrap.html(html);
+                    profAnim();
+                }
+            });
+        });
+    });
+
+    $('#resume').click(function (e) {
+        e.preventDefault();
+
+        $content.fadeOut(300, function () {
+            $.ajax({
+                url: "https://trufinol.github.io/VCard/Projects/resume.html",
+                success: function (html) {
+                    $contwrap.html(html);
+                    $content.fadeIn(300);
+
+                }
+            });
+            $(document).ajaxComplete(function () {
+
+                function barAnimate(id) {
+
+                    idVal = id.data('value');
+                    TweenMax.to(id, 1, { width: idVal + '%', ease: Power1.easeIn });
+
+                    setInterval(function () {
+                        var progr = $('.progress').css('width');
+                        var length = id.css('width'),
+                        perc = Math.round((parseInt(length) / parseInt(progr)) * 100);
+                        id.text(perc + '%');
+                    }, 50);
+                }
+                if (window.innerHeight < 800) {
+                $(document).bind('scroll', function (e) {
+                    var scrollOffset = $(document).scrollTop();
+                    var containerOffset = $('#skillbars').offset().top - window.innerHeight;
+                    if (scrollOffset > containerOffset) {
+                        barAnimate($('#bar-1'));
+                        barAnimate($('#bar-2'));
+                        barAnimate($('#bar-3'));
+                        barAnimate($('#bar-4'));
+                        $(document).unbind('scroll');
+                    }
+                });
+                } else {
+                    barAnimate($('#bar-1'));
+                    barAnimate($('#bar-2'));
+                    barAnimate($('#bar-3'));
+                    barAnimate($('#bar-4'));
+                }
+
+                function educAnimate(id) {
+
+                    TweenMax.fromTo(id, 0.5, { x: -800 }, { x: 0, ease: Sine.easeOut }, 0.3);
+
+                }
+                educAnimate($('.topic, h2'));
+            });
+
+        });
+    });
+
+    $('#portfolio').click(function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: "https://trufinol.github.io/VCard/Projects/portfolio.html",
+            success: function (html) {
+                $contwrap.html(html);
+
+            }
+        });
+    });
+    $('#contacts').click(function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: "https://trufinol.github.io/VCard/Projects/contacts.html",
+            success: function (html) {
+                $contwrap.html(html);
+
+            }
+        });
+    });
+
+});
