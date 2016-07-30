a4c4b830cc532e2dd5f1fa7ea26d426c3fad88e0
Nikolay <veseliy07@gmail.com>
2016-07-30 18:49:53 +0300

    onlady markup

---
+++ onlady/js/sliders.js
@@ -0,0 +1,48 @@
+
+window.onload = function () {
+
+  var fullScreenSwiper = new Swiper('.wide-swiper', {
+    pagination: '.wide-swiper__pagination',
+    paginationClickable: true,
+
+  });
+
+  var mainSwiper = new Swiper('.m-swiper-container', {
+    loop: true,
+    slidesPerView: 6,
+    spaceBetween: 30,
+    nextButton: '.m-swiper__prev',
+    prevButton: '.m-swiper__next',
+    breakpoints: {
+            1200: {
+                slidesPerView: 5,
+                spaceBetween: 40
+            },
+            992: {
+                slidesPerView: 4,
+                spaceBetween: 20
+            },
+            770: {
+                slidesPerView: 3,
+                spaceBetween: 20
+            }
+          }
+  });
+
+  var secondary1Swiper = new Swiper('.sec1-swiper-container', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 30,
+    nextButton: '.sec1-swiper__prev',
+    prevButton: '.sec1-swiper__next'
+  });
+
+  var secondary2Swiper = new Swiper('.sec2-swiper-container', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 30,
+    nextButton: '.sec2-swiper__prev',
+    prevButton: '.sec2-swiper__next'
+  });
+
+};
